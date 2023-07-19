4c7807b236b92db06f0fb9a47685598aab4056f6
Mateen <mateenqazi125@gmail.com>
2023-07-19 15:44:02 +0500

    implement data models and db configurations

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"clockify/storage"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+
+	db, err := storage.NewConnection(config)
+	if err != nil {
+		log.Fatal("could not load the database")
+	}
+
+	log.Println(db)
+
+	app := fiber.New()
+	app.Listen(":8080")
 }
