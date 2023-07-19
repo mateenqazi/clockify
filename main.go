3c31ca8fa6d2199d79dbf41aae11ae7d56e9a843
Mateen <mateenqazi125@gmail.com>
2023-07-19 13:45:05 +0500

    docker implementation

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"clockify/activities"
 	"clockify/helpers"
-	"clockify/projects"
 	"clockify/storage"
-	"clockify/types"
-	"clockify/users"
-	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -40,127 +34,127 @@ func main() {
 	}
 
 	// initialization services
-	userService := users.NewUserService(db)
-	projectService := projects.NewProjectService(db)
-	activitiesService := activities.NewActivitiesService(db)
+	// userService := users.NewUserService(db)
+	// projectService := projects.NewProjectService(db)
+	// activitiesService := activities.NewActivitiesService(db)
 
-	// register user
-	creds := types.Credentials{
-		Email:    "mat1@gmail.com",
-		Password: "securepassword",
-	}
+	// // register user
+	// creds := types.Credentials{
+	// 	Email:    "mat1@gmail.com",
+	// 	Password: "securepassword",
+	// }
 
-	helpers.FormatMessage("Register Service Started")
+	// helpers.FormatMessage("Register Service Started")
 
-	result, err := userService.RegisterUser(creds)
-	if err != nil {
-		fmt.Println("Error rise Register User ", err)
-	}
+	// result, err := userService.RegisterUser(creds)
+	// if err != nil {
+	// 	fmt.Println("Error rise Register User ", err)
+	// }
 
-	fmt.Println(result)
+	// fmt.Println(result)
 
-	helpers.FormatMessage("Register Service Ended")
+	// helpers.FormatMessage("Register Service Ended")
 
-	// login user
-	helpers.FormatMessage("Login Service Started")
+	// // login user
+	// helpers.FormatMessage("Login Service Started")
 
-	creds = types.Credentials{
-		Email:    "mat1@gmail.com",
-		Password: "securepassword",
-	}
+	// creds = types.Credentials{
+	// 	Email:    "mat1@gmail.com",
+	// 	Password: "securepassword",
+	// }
 
-	user, err := userService.LoginUser(creds)
-	if err != nil {
-		panic(err)
-	}
+	// user, err := userService.LoginUser(creds)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	fmt.Println("you are login with ", user.Email)
+	// fmt.Println("you are login with ", user.Email)
 
-	helpers.FormatMessage("Login Service Ended")
+	// helpers.FormatMessage("Login Service Ended")
 
-	helpers.FormatMessage("Create New Project Service Started")
+	// helpers.FormatMessage("Create New Project Service Started")
 
-	project := types.Project{
-		Name:       "Project 9",
-		ClientName: "Test Client",
-		UserId:     user.ID,
-	}
+	// project := types.Project{
+	// 	Name:       "Project 9",
+	// 	ClientName: "Test Client",
+	// 	UserId:     user.ID,
+	// }
 
-	proj, err := projectService.CreateNewProject(project)
-	if err != nil {
-		fmt.Println("Error rise Register User ", err)
-	}
+	// proj, err := projectService.CreateNewProject(project)
+	// if err != nil {
+	// 	fmt.Println("Error rise Register User ", err)
+	// }
 
-	fmt.Println(proj)
+	// fmt.Println(proj)
 
-	helpers.FormatMessage("Create New Project Service Ended")
+	// helpers.FormatMessage("Create New Project Service Ended")
 
-	helpers.FormatMessage("Search Project Service Started")
+	// helpers.FormatMessage("Search Project Service Started")
 
-	searchKeyword := "8"
-	searchResult, err := projectService.SearchProject(searchKeyword, user.ID)
-	if err != nil {
-		fmt.Println("Error rise Register User ", err)
-	}
+	// searchKeyword := "8"
+	// searchResult, err := projectService.SearchProject(searchKeyword, user.ID)
+	// if err != nil {
+	// 	fmt.Println("Error rise Register User ", err)
+	// }
 
-	fmt.Println(searchResult)
+	// fmt.Println(searchResult)
 
-	helpers.FormatMessage("Search Project Service Ended")
+	// helpers.FormatMessage("Search Project Service Ended")
 
-	helpers.FormatMessage("Create Activities Service Started")
-	act := types.Activities{
-		Name:      "Second Activity",
-		StartTime: time.Now(),
-		EndTime:   time.Now().Add(2 * time.Hour),
-	}
-	act.TimeDuration = act.EndTime.Sub(act.StartTime)
-	act.UserId = user.ID
-	act.ProjectId = 1
+	// helpers.FormatMessage("Create Activities Service Started")
+	// act := types.Activities{
+	// 	Name:      "Second Activity",
+	// 	StartTime: time.Now(),
+	// 	EndTime:   time.Now().Add(2 * time.Hour),
+	// }
+	// act.TimeDuration = act.EndTime.Sub(act.StartTime)
+	// act.UserId = user.ID
+	// act.ProjectId = 1
 
-	check, err := activitiesService.CreateActivities(act)
-	if err != nil {
-		fmt.Println("Error Rise Create Activities ", err)
-	}
+	// check, err := activitiesService.CreateActivities(act)
+	// if err != nil {
+	// 	fmt.Println("Error Rise Create Activities ", err)
+	// }
 
-	fmt.Println(check)
-	helpers.FormatMessage("Create Activities Service Ended")
+	// fmt.Println(check)
+	// helpers.FormatMessage("Create Activities Service Ended")
 
-	helpers.FormatMessage("Duplicate Activities Service Started")
+	// helpers.FormatMessage("Duplicate Activities Service Started")
 
-	duplicate, err := activitiesService.DuplicateActivity(5)
-	if err != nil {
-		fmt.Println("Error Rise Duplicate Activities ", err)
-	}
+	// duplicate, err := activitiesService.DuplicateActivity(5)
+	// if err != nil {
+	// 	fmt.Println("Error Rise Duplicate Activities ", err)
+	// }
 
-	fmt.Println(duplicate)
-	helpers.FormatMessage("Duplicate Activities Service Ended")
+	// fmt.Println(duplicate)
+	// helpers.FormatMessage("Duplicate Activities Service Ended")
 
-	helpers.FormatMessage("Delete Project Service Started")
+	// helpers.FormatMessage("Delete Project Service Started")
 
-	if len(searchResult) > 0 {
-		deleteSuccessfully, err := projectService.DeleteProject(searchResult[0].Name, user.ID)
-		if err != nil {
-			fmt.Println("Error rise Delete Project ", err)
-		}
+	// if len(searchResult) > 0 {
+	// 	deleteSuccessfully, err := projectService.DeleteProject(searchResult[0].Name, user.ID)
+	// 	if err != nil {
+	// 		fmt.Println("Error rise Delete Project ", err)
+	// 	}
 
-		fmt.Println(deleteSuccessfully)
-	}
+	// 	fmt.Println(deleteSuccessfully)
+	// }
 
-	helpers.FormatMessage("Delete Project Service Ended")
+	// helpers.FormatMessage("Delete Project Service Ended")
 
-	helpers.FormatMessage("Delete Activities Service Started")
+	// helpers.FormatMessage("Delete Activities Service Started")
 
-	del, err := activitiesService.DeleteActivity(2)
-	if err != nil {
-		fmt.Println("Error Rise Delete Activities ", err)
-	}
+	// del, err := activitiesService.DeleteActivity(2)
+	// if err != nil {
+	// 	fmt.Println("Error Rise Delete Activities ", err)
+	// }
 
-	fmt.Println(del)
+	// fmt.Println(del)
 
-	helpers.FormatMessage("Delete Activities Service Ended")
+	// helpers.FormatMessage("Delete Activities Service Ended")
 
 	// migration
-	// helpers.MigrateTable(db)
+	helpers.MigrateTable(db)
 
 	app := fiber.New()
 	app.Listen(":8080")
