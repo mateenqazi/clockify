2e5b0cd5da516f8e4c3837b87d0349479955471d
Mateen <mateenqazi125@gmail.com>
2023-07-20 18:22:27 +0500

    user module complete

--- users/controller.go
+++ users/controller.go
@@ -11,8 +11,8 @@ func UserSubrouter(r *mux.Router, db *gorm.DB) {
 
 	userService := NewUserService(db)
 	userRouter := r.PathPrefix("/users").Subrouter()
-	// userRouter.HandleFunc("/create", createUserHandler).Methods(http.MethodPost)
-	// userRouter.HandleFunc("/delete", createUserHandler).Methods(http.MethodDelete)
-	// userRouter.HandleFunc("/login", userService.GetAllUser).Methods(http.MethodPost)
+	userRouter.HandleFunc("/create", userService.RegisterUser).Methods(http.MethodPost)
+	userRouter.HandleFunc("/delete/{id}", userService.DeleteUser).Methods(http.MethodDelete)
+	userRouter.HandleFunc("/login", userService.LoginUser).Methods(http.MethodPost)
 	userRouter.HandleFunc("/get", userService.GetAllUser).Methods(http.MethodGet)
 }
